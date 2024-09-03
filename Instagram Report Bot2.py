5956dbb17252dde27efafba20ce67c7b6851607d
t88848 <t8844vato@yahoo.com>
2024-09-03 11:21:17 -0400

    Update Instagram Report Bot2.py

--- Instagram Report Bot2.py
+++ Instagram Report Bot2.py
@@ -9,7 +9,7 @@ import sys
 def getOptions(args=sys.argv[1:]):
 
     parser = argparse.ArgumentParser(description="This bot helps users to mass report accounts with clickbaits or objectionable material.")
-    parser.add_argument("-u", "--username", type = str, default = "", help = "Username to report.")
+    parser.add_argument("-u", "--username", type = str, default = "", help = "quo7te")
     parser.add_argument("-f", "--file", type = str, default = "acc.txt", help = "Accounts list ( Defaults to acc.txt in program directory ).")
 
     options = parser.parse_args(args)
@@ -43,11 +43,11 @@ for line in range(len(file)+1):
     web = Browser()
     web.go_to("https://www.instagram.com/accounts/login/")
 
-    web.type(user[line], into='flarm06@gmail.com')
+    web.type(user[line], into='t8844_')
     time.sleep(0.5)
     web.press(web.Key.TAB)
     time.sleep(0.5)
-    web.type(passw[line], into='Grimboide06')
+    web.type(passw[line], into='LoTo2004'?'@'!')
     web.press(web.Key.ENTER)
 
     time.sleep(2.0)
